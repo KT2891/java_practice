f7272d9f9486cd8303227250986d93b02c5ff8c9
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-20 14:14:06 +0900

    練習問題9

---
+++ practice1/src/practice1/test9.java
@@ -0,0 +1,13 @@
+package practice1;
+
+public class test9 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 5;
+		for (int i=2; i<=4; i++) {
+			System.out.println(x * i);
+		}
+	}
+
+}
