e0f14bb8f041bfa66778f061eab27003d17f60e8
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-20 13:37:20 +0900

    Signed-off-by: Keishiro Takata <t.k.6.2.1995@gmail.com>

---
+++ practice1/src/practice1/test1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package practice1;
+
+/**
+ * 
+ */
+public class test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 0; i < 5; i++) {
+			System.out.println("---");
+			System.out.println(i);
+		}
+	}
+
+}
