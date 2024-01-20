371cd35a2730053f12c32a78ecdac42a15e08d49
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-20 15:59:56 +0900

    練習問題3-1

---
+++ practice1/src/practice3/test1.java
@@ -0,0 +1,15 @@
+package practice3;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x, y;
+		x = 30;
+		y = 20;
+		if (x > y) {
+			System.out.println(x + "は" + y + "より大きい");
+		}
+	}
+
+}
