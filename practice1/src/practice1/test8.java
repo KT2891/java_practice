567b4042dd8fcfc45db5acaa9527017e67330afe
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-20 14:12:27 +0900

    練習問題8

---
+++ practice1/src/practice1/test8.java
@@ -0,0 +1,14 @@
+package practice1;
+
+public class test8 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 19;
+		int y = 23;
+		int z;
+		z = x * y;
+		System.out.println("z=" + z);
+	}
+
+}
