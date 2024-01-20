163eb78ba33b7404f8d34224616a020ead5857fb
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-20 14:20:17 +0900

    練習問題10

--- practice1/src/practice1/test10.java
+++ practice1/src/practice1/test10.java
@@ -4,7 +4,12 @@ public class test10 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		int x = 5;
+		int result = 1;
+		for (int i=1; i <= 3; i++) {
+			result *= x;
+			System.out.println(x + "の" + i + "乗は" + result);
+		}
 	}
 
 }
