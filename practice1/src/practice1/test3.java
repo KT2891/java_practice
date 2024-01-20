0ccd2b628ca476a75c9c618fbd32ce8c67343c02
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-20 14:05:24 +0900

    練習問題3

---
+++ practice1/src/practice1/test3.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 13;
+		int y = 17;
+		System.out.println("x=" + x + "," + "y=" + y);
+	}
+
+}
