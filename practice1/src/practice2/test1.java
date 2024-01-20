21ba6a1a96c0b964d8ac32e0b7fd6c9279c55aa9
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-20 14:35:52 +0900

    練習問題2-1

---
+++ practice1/src/practice2/test1.java
@@ -0,0 +1,18 @@
+package practice2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test1 {
+
+	public static void main(String[] args) throws IOException
+	{
+		// TODO 自動生成されたメソッド・スタブ
+    BufferedReader br = new BufferedReader(
+        new InputStreamReader( System.in ) );
+
+    String  s = br.readLine();    
+    System.out.println(s);
+	}
+
+}
