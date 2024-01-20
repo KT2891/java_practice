cd0a82f4a9e8efaaf30c4a5958d50e300cda5ab5
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-20 14:11:24 +0900

    練習問題7

--- practice1/src/practice1/test7.java
+++ practice1/src/practice1/test7.java
@@ -4,7 +4,13 @@ public class test7 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		int x = 3;
+		int y = 7;
+		int z;
+		z = x;
+		x = y;
+		y = z;
+		System.out.println("x=" + x + "," + "y=" + y);
 	}
 
 }
