3e0679792ef00bc21a46cc83fb428252d21d8d92
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-20 14:27:22 +0900

    練習問題12

---
+++ practice1/src/practice1/test12.java
@@ -0,0 +1,18 @@
+package practice1;
+
+public class test12 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x = 10;
+		System.out.println("インクリメント演算子を使用した場合");
+		for (int i = 0; i <= 5; i++) {
+			System.out.println(x + i);
+		}
+		System.out.println("インクリメント演算子を使用した場合");
+		for (int i = 0; i >= -5; i--) {
+			System.out.println(x + i);
+		}
+	}
+
+}
