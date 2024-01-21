4a75a0b19fc43035555c86a50af41f60fee191fe
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-21 15:33:35 +0900

    練習問題4-1

---
+++ practice1/src/practice4/test01.java
@@ -0,0 +1,12 @@
+package practice4;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i = 0; i < 10; i++) {
+			System.out.println("SPAM");
+		}
+	}
+
+}
---
+++ practice1/src/practice4/test02.java
@@ -0,0 +1,10 @@
+package practice4;
+
+public class test02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
