036e13c19e4052de74aeb61c2e0c3e218b4919ae
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-21 15:38:18 +0900

    練習問題4-3

---
+++ practice1/src/practice4/test03.java
@@ -0,0 +1,14 @@
+package practice4;
+
+public class test03 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int result = 1;
+		for (int i = 1; i <= 8; i++) {
+			result *= 2;
+			System.out.println("2の"+ i + "乗は " + result);
+		}
+	}
+
+}
