d01cfea6cc027ff615fcaaf5fa02b950518ee1a5
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-21 15:41:02 +0900

    練習問題4-4

---
+++ practice1/src/practice4/test04.java
@@ -0,0 +1,13 @@
+package practice4;
+
+public class test04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int result = 1;
+		for (int i = 1; i <= 7; i++)
+			result *= i;
+		System.out.println(result);
+	}
+
+}
