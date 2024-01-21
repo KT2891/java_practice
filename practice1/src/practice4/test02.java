962435768fbe18e033581ed1b2522e0e39d07978
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-21 15:35:40 +0900

    練習問題4-2

--- practice1/src/practice4/test02.java
+++ practice1/src/practice4/test02.java
@@ -4,7 +4,9 @@ public class test02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		for (int i = 1; i <= 9; i++) {
+			System.out.println("3 ✕ " + i + " = " + 3*i);
+		}
 	}
 
 }
