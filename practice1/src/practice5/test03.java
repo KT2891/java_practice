4c3f700c0c464a1d25b9cff5624e3e8d8b744c8c
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-22 17:02:34 +0900

    練習問題5-3

--- practice1/src/practice5/test03.java
+++ practice1/src/practice5/test03.java
@@ -3,8 +3,17 @@ package practice5;
 public class test03 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		int[] n = {1,2,3,4,5,6,7,8,9,10};
+		System.out.print("偶数：");
+		for (int i = 0; i <= 9; i++) {
+			if (n[i] % 2 == 0)
+				System.out.print(n[i] + " ");
+		}
+		System.out.println();
+		System.out.print("奇数：");
+		for (int i = 0; i <= 9; i++) {
+			if (n[i] % 2 != 0)
+				System.out.print(n[i] + " ");
+		}
 	}
-
 }
