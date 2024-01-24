795b631a315635096c8c1ba8d836d82f6a1ea699
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-24 20:45:59 +0900

    練習問題8

--- practice1/src/practice6/test08.java
+++ practice1/src/practice6/test08.java
@@ -3,8 +3,17 @@ package practice6;
 public class test08 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		for(int n = 11; n <= 20; n++)
+			System.out.println(Fibonacci(n));
 
 	}
-
+	private static int Fibonacci(int n) {
+		if(n == 0) {
+			return 0;
+		} else if(n == 1) {
+			return 1;
+		} else {
+			return Fibonacci( n - 2 ) + Fibonacci( n - 1);
+		}			
+	}
 }
