aa57d30dc08e56b0e9f6814b1cbb26ce8e38c6b4
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-24 19:34:59 +0900

    練習問題6-2

--- practice1/src/practice6/test02.java
+++ practice1/src/practice6/test02.java
@@ -1,10 +1,22 @@
 package practice6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class test02 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ));
+		System.out.print("1つめの数値を入力：");
+		int first = Integer.parseInt( br.readLine());
+		System.out.print("2つめの数値を入力：");
+		int second = Integer.parseInt( br.readLine());
+		System.out.println(first + "と" + second + "の平均は" + ave(first, second));
+	}
+	
+	private static int ave(int f, int c) {
+		return (f + c) / 2;
 	}
 
 }
