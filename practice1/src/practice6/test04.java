fe584a763c574cc63612ad306bf129adb98c4ddd
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-24 20:18:05 +0900

    練習問題4

--- practice1/src/practice6/test04.java
+++ practice1/src/practice6/test04.java
@@ -4,7 +4,17 @@ public class test04 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		delta(3);
+		System.out.println();
+		delta(4);
+		System.out.println();
+		delta(5);
 	}
-
+	private static void delta(int n) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++)
+				System.out.print("$");
+			System.out.println();
+		}
+	}
 }
