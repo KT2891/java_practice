a85b98c4d0b17cf5fbe9867b59c655dd81f34a52
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-24 20:22:57 +0900

    練習問題6

--- practice1/src/practice6/test06.java
+++ practice1/src/practice6/test06.java
@@ -4,7 +4,13 @@ public class test06 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		for(int i = 1; i <= 9; i++)
+			kuku(i);
 	}
-
+	
+	private static void kuku(int n) {
+		for(int i = 1; i <= 9; i++)
+			System.out.printf(" %2d", i * n);
+		System.out.println();
+	}
 }
