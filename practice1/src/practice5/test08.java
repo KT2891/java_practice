0069271de9d66df517cb774a53fe1d24138f8930
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-24 19:19:24 +0900

    練習問題8

--- practice1/src/practice5/test08.java
+++ practice1/src/practice5/test08.java
@@ -1,10 +1,27 @@
 package practice5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class test08 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ));
+		int n[] = new int[10];
+		for (int i = 0; i < 10; i++)
+			n[i] = Integer.parseInt( br.readLine() );
+		
+		int baf = 0;
+		for (int j = 0; j< 9; j++)
+			for(int k = 0; k < 9; k++)
+				if (n[k] < n[k + 1]) {
+					baf = n[k + 1];
+					n[k + 1] = n[k];
+					n[k] = baf;
+				} 
+		for (int data : n)
+			System.out.print(data + " ");
 	}
 
 }
