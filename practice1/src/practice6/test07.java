e313eac65054ba4fba399720c694bf6b1e2de439
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-24 20:29:21 +0900

    練習問題7

--- practice1/src/practice6/test07.java
+++ practice1/src/practice6/test07.java
@@ -3,8 +3,21 @@ package practice6;
 public class test07 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		for (int i = 1; i <= 10100; i++) {
+			sosuu(i);
+		}
 
 	}
-
+	private static void sosuu(int n) {
+		if (n <= 3) {
+			System.out.print(n + " ");
+			return;
+		}
+		for (int i = 2; i <= n/2; i++) {
+			if((n % i) == 0){
+				return;
+			}
+		}
+		System.out.print(n + " ");
+	}
 }
