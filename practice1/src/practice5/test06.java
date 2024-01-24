ca3dc6a46529f1ad21b9b2d95ee2da35487a655d
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-24 19:23:38 +0900

    修正

--- practice1/src/practice5/test06.java
+++ practice1/src/practice5/test06.java
@@ -1,14 +1,8 @@
 package practice5;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class test06 {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(
-        new InputStreamReader( System.in ) );
+	public static void main(String[] args){
 		int kuku[][] = new int[9][];
 		
 		for( int i = 0 ; i < 9 ; i++ ) {
