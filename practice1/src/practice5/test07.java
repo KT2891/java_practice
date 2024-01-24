d1ae494a4b753b184ec811aadeca99579ef0f6d0
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-24 19:03:20 +0900

    練習問題

--- practice1/src/practice5/test07.java
+++ practice1/src/practice5/test07.java
@@ -1,10 +1,23 @@
 package practice5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class test07 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+		int kuku[][] = new int[9][9];
+		System.out.print("1つめの数値：");
+		int first = Integer.parseInt( br.readLine() );
+		System.out.print("2つめの数値：");
+		int second = Integer.parseInt( br.readLine() );
+		for(int i = 0; i < 9; i++)
+			for(int j = 0; j < 9; j++)
+				kuku[i][j] = (i + 1)*(j + 1);
+		
+		System.out.println(first + "✕" + second + "=" + kuku[first - 1][second - 1]);
 	}
 
 }
