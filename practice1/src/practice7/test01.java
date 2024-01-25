bc3154170e9d89db45ac35cff5be327f0be67be7
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-25 15:52:30 +0900

    練習問題7-1

---
+++ practice1/src/practice7/test01.java
@@ -0,0 +1,12 @@
+package practice7;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Dog dog1 = new Dog();
+		dog1.SetName("ポチ");
+		dog1.ShowProfile();
+	}
+
+}
