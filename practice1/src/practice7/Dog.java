6fe59bdc7ea88f9bcb0ba517046727458d74f9e9
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-25 15:57:41 +0900

    練習問題7-2

--- practice1/src/practice7/Dog.java
+++ practice1/src/practice7/Dog.java
@@ -3,13 +3,20 @@ package practice7;
 public class Dog {
 
 	private String Name;
+	private int Age;
 	
 	public void SetName( String nm ) {
 		Name = nm;
 	}
 	
+	public void SetAge( int age) {
+		Age = age;
+	}
+	
 	public void ShowProfile() {
 		System.out.println( "名前は" + Name + "です。" );
+		if (Age != 0)
+			System.out.println( "年齢は" + Age + "です。" );
 	}
 
 }
---
+++ practice1/src/practice7/test02.java
@@ -0,0 +1,13 @@
+package practice7;
+
+public class test02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Dog dog2 = new Dog();
+		dog2.SetName("シロ");
+		dog2.SetAge(4);
+		dog2.ShowProfile();
+	}
+
+}
