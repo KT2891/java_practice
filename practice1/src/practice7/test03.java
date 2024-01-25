5bfc547a9d8b48fb8aaa14273d2bf07b1e5e25ae
Keishiro Takata <t.k.6.2.1995@gmail.com>
2024-01-25 16:00:54 +0900

    練習問題7-3

---
+++ practice1/src/practice7/test03.java
@@ -0,0 +1,16 @@
+package practice7;
+
+public class test03 {
+
+	public static void main(String[] args) {
+		Dog dog3_1 = new Dog();
+		Dog dog3_2 = new Dog();
+		dog3_1.SetName("アイ");
+		dog3_2.SetName("ウエ");
+		dog3_1.SetAge(4);
+		dog3_2.SetAge(6);
+		dog3_1.ShowProfile();
+		dog3_2.ShowProfile();
+	}
+
+}
